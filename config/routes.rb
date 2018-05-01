40599f0bcd78808dd44daa4f6f1db6064f0c3e01
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-05-01 10:49:13 +0900

    create view update routes
    question new, users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "questions#index"
+  resources :questions, path: 'ja/questions'
+  resources :users
 end
