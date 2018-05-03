cd21468c42b1f5a32189466d6b8ac85e08ca56dc
fusemawari <fusemawari@gmail.com>
2018-05-03 19:22:47 +0900

    add_views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "chats#index"
-  resources :chats, path: 'ja/chats'
+  root "tops#index"
+  resources :categories, path: '/ja'
+  resources :chats
+
 end
