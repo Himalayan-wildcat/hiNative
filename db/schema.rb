d7c1863a71cd66df2828afd581187bc5741d121f
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-05-03 18:53:43 +0900

    update view registrations new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503033140) do
+ActiveRecord::Schema.define(version: 20180503080947) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "list", null: false
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 20180503033140) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin_flg"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
