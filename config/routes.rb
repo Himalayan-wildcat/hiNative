20e6b3e73a885bc8886447b1371e7161aa5a9c78
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-05-04 12:37:14 +0900

    fix view categories index settings show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "tops#index"
-  resources :categories, path: '/ja'
+  resources :categories, path: '/ja', only: :index
   resources :chats
-  resources :users, except: [:new, :create], path: '/ja/users/profile'
+  resources :users, except: [:new, :create], path: '/ja/users/profiles'
   resource  :settings, except: [:new, :create], path: 'ja/settings'
 end
