9fb11d71d564138e532b93d0c9155668944528e6
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-05-04 12:08:55 +0900

    create routing users profile settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tops#index"
   resources :categories, path: '/ja'
   resources :chats
-
+  resources :users, except: [:new, :create], path: '/ja/users/profile'
+  resource  :settings, except: [:new, :create], path: 'ja/settings'
 end
