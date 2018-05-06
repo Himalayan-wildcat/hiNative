74bfc33025645a64071b5ee5e9ed144ff7c64afa
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-05-06 11:08:33 +0900

    remove cancancnan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503080947) do
+ActiveRecord::Schema.define(version: 20180506014028) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "list", null: false
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20180503080947) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin_flg"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
