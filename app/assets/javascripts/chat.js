2a28d9958cd83223eb0552c30bf66466ac70486b
NateRive <naterive1125@gmail.com>
2018-05-08 23:52:26 +0900

    Improve a little

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,10 +1,10 @@
 $(function() {
   if ($("input[type='hidden']")[0]) {
     bottom();
-      var userId = $('input[type="hidden"]').data("userid");
+      var targetId = $('input[type="hidden"]').data("roomid");
       App.chat = App.cable.subscriptions.create({
         channel: "ChatChannel",
-        userId: userId
+        userId: targetId
       }, {
         connected: function() {
           console.log("connected");
@@ -14,12 +14,13 @@ $(function() {
         },
         received: function(data) {
           if (data['message']) {
-            var user_id = $('input[type="hidden"]').val() == data['user_id'] ? "__right" : "__left"
+            var user_id, user_name, rest;
+            [user_id, user_name] = $('input[type="hidden"]').val() == data['user_id'] ? ["__right", "自分"] : ["__left", data['user_name']]
                 var html = `
                 <div class="message-box${user_id}">
                     <li class="messages__list">
                       <span class="messages__name">
-                        ${data['user_name']}
+                        ${user_name}
                       </span>
                       <span class="messages__date">
                         ${data['message']['created_at']}
@@ -37,8 +38,9 @@ $(function() {
             $(".error_message").css("display", "block");
             $(".error_message").text(data['error']);
           }
+          bottom();
         },
-        speak: function(message) {
+        speak: function(message, userId) {
           return this.perform('speak', {
             message: message,
             userId: userId
@@ -60,7 +62,8 @@ $(function() {
           $(".error_message").css("display", "none");
         }
         if (event.keyCode == 13) {
-          App.chat.speak(event.target.value);
+          var userId = $('input[type="hidden"]').data("userid");
+          App.chat.speak(event.target.value, userId);
           event.target.value = "";
           event.preventDefault();
           bottom();
@@ -69,7 +72,8 @@ $(function() {
 
       $(".submitBtn").on("click", function() {
         var content = $('[data-behavior=chat_room]').val();
-        App.chat.speak(content);
+        var userId = $('input[type="hidden"]').data("userid");
+        App.chat.speak(content, userId);
         $('[data-behavior=chat_room]').val("");
         bottom();
       });
