18c010a0e6144692ad140484ef3a736d26f367f3
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-05-09 11:47:12 +0900

    create chat link

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,10 @@ module Admin
   class UsersController < Admin::ApplicationController
     # To customize the behavior of this controller,
     # you can overwrite any of the RESTful actions. For example:
-    #
+    # def index
+    #     super
+    #     @resources = User.all
+    # end
     # def index
     #   super
     #   @resources = User.
